943c79025194fbbf22376879a537d19917d9f4ef
Ferenc Janky <fecjanky@gmail.com>
2018-04-29 12:56:52 +0200

    working solution with numpy

--- toki1.py
+++ toki1.py
@@ -1,12 +1,16 @@
 import time
+import itertools
+# import matplotlib
+# matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy.stats as stats
 import seaborn as sns
 import argparse
+from datetime import datetime, timedelta
 
-#Feladat
+# Feladat
 """Homework:
 paper: http://hsnlab.tmit.bme.hu/%7Emolnar/files/ilk97ext.pdf
 
@@ -14,7 +18,7 @@ Perform a similar traffic analysis study that is in the first paper with an arbi
 (It can be measured internet traffic from anywhere from the internet, e.g., http://ita.ee.lbl.gov/ or it can be your 
 own measured traffic or even other types of time series.)"""
 
-#Time series chosen:
+# Time series chosen:
 """
 http://ita.ee.lbl.gov/html/contrib/LBL-TCP-3.html
 The trace ran from 14:10 to 16:10 on Thursday, January 20, 1994 (times are Pacific Standard Time), capturing 
@@ -24,56 +28,77 @@ trace was made using tcpdump on a Sun Sparcstation using the BPF kernel packet f
 Timestamps have microsecond precision. """
 
 
+def parse_datetime(datetimestring):
+    return datetime.strptime(datetimestring, "%Y.%m.%d %H:%M:%S.%f")
+
+
+def test_parse_datetime():
+    date = parse_datetime("2018.03.18 08:21:33.420")
+    assert date.year == 2018
+    assert date.month == 3
+    assert date.day == 18
+    assert date.hour == 8
+    assert date.minute == 21
+    assert date.second == 33
+    assert date.microsecond == 420000
+    pass
+
+
 def parsefile(filename):
     with open(filename) as f:
-        for line in f:
-            lines = line.rstrip('\n')
-            columns = lines.split()
-            timestamps.append(float(columns[0]))
-            data.append(float(columns[5]))
+        timestamps = [parse_datetime(line.strip()) for line in f if line.strip()]
+        t0 = min(timestamps)
+        nptimestamps = np.fromiter(((t - t0).total_seconds() for t in timestamps),np.float64,len(timestamps))
+        return np.sort(nptimestamps)
 
 
 def getinterarrivals(arrivals):
-    last = 0
-    for x in arrivals:
-        previous = last
-        last = x
-        if (previous != 0):
-            interarrivals.append(last - previous)
+    if len(arrivals) is 0:
+        return [0]
+    i = 1
+    while i < len(arrivals):
+        yield arrivals[i] - arrivals[i - 1]
+        i = i + 1
 
 
 def plotinterarrivalpdf(interarr):
-    sns.distplot(interarr,100)
+    sns.distplot(interarr, 100)
     plt.xlabel('interarrival length')
     plt.ylabel('pdf')
     plt.show()
 
 
-def plotintensity(timearray):
-    n = 1
-    for t in timearray:
-        packetcounter[n-1] += 1
-        if (t > n):
-            n += 1
-            packetcounter.append(0)
-    x_intensity = np.linspace(0,n,7200)
-    plt.plot(x_intensity,packetcounter)
+def getintensity(timelist):
+    n = 0
+    packetcounter = [0]
+    for t in timelist:
+        if t - timelist[0] > n + 1:
+            diff = t - timelist[0]
+            n += diff
+            packetcounter += itertools.repeat(0, int(diff + 1))
+        packetcounter[int(n)] += 1
+    return packetcounter
+
+
+def plotintensity(packetcounter):
+    plt.plot(range(0, len(packetcounter)), packetcounter)
     plt.xlabel('time')
     plt.ylabel('intensity')
     plt.show()
 
 
 def autocorr(x, lag):
-    return np.corrcoef(np.array([x[0:len(x)-lag],x[lag:len(x)]]))
+    # return np.correlate(x[0:len(x)-lag],x[0:len(x)-lag])[0]
+    return np.corrcoef(x[0:len(x) - lag], x[lag:len(x)])
+    # return np.corrcoef(np.array([x[0:len(x) - lag], x[lag:len(x)]]))
 
 
 def plotarrtimecorrelation(interarrivals):
     y_arrtimecorrelation = []
-    for lag in range (0,500):
-        acorr = autocorr(interarrivals,lag)
-        y_arrtimecorrelation.append(acorr[0,1])
-    x_arrtimecorrelation = np.linspace(0,500,500)
-    plt.plot(x_arrtimecorrelation,y_arrtimecorrelation)
+    for lag in range(0, 500):
+        y_arrtimecorrelation.append(autocorr(interarrivals, lag)[0, 1])
+    x_arrtimecorrelation = np.linspace(0, 500, 500)
+    plt.plot(x_arrtimecorrelation, y_arrtimecorrelation)
     plt.xlabel('lag')
     plt.ylabel('correlation')
     plt.show()
@@ -81,18 +106,17 @@ def plotarrtimecorrelation(interarrivals):
 
 def plotpacketcountcorrelation(packetcount):
     y_packetcountcorrelation = []
-    for lag in range (0,500):
-        acorr = autocorr(packetcount, lag)
-        y_packetcountcorrelation.append(acorr[0,1])
-    x_packetcountcorrelation = np.linspace(0,500,500)
-    plt.plot(x_packetcountcorrelation,y_packetcountcorrelation)
+    for lag in range(0, 500):
+        y_packetcountcorrelation.append(autocorr(packetcount, lag)[0,1])
+    x_packetcountcorrelation = np.linspace(0, 500, 500)
+    plt.plot(x_packetcountcorrelation, y_packetcountcorrelation)
     plt.xlabel('lag')
     plt.ylabel('correlation')
     plt.show()
 
 
-#General statistics
-#--------------------
+# General statistics
+# --------------------
 """def mean(arr):
     summa = 0
     for i in arr:
@@ -101,48 +125,48 @@ def plotpacketcountcorrelation(packetcount):
     return mean"""
 
 
-#peak to mean ratio
+# peak to mean ratio
 def peaktomean(interarr):
     peak = max(interarr)
-    return peak/np.mean(interarr)
+    return peak / np.mean(interarr)
 
 
-#squared coefficient variation
+# squared coefficient variation
 def scv(interarr):
-    return np.var(interarr)/(np.mean(interarr) * np.mean(interarr))
+    return np.var(interarr) / (np.mean(interarr) * np.mean(interarr))
 
 
-#skewness
+# skewness
 def thirdmoment(interarr):
     interarray = np.array(interarr)
     return stats.skew(interarray)
 
 
-#i is arbitrary
-def idi(interarr,k):
+# i is arbitrary
+def idi(interarr, k):
     sumlist = []
-    for i in range(0,len(interarr)-k):
+    for i in range(0, len(interarr) - k):
         summa = 0
-        for j in range(i,i+k):
+        for j in range(i, i + k):
             summa += interarr[j]
         sumlist.append(summa)
     variance = np.var(sumlist)
-    idival = variance/(k * np.mean(interarr) * np.mean(interarr))
+    idival = variance / (k * np.mean(interarr) * np.mean(interarr))
     return idival
 
 
-def plotidi(interarr,k):
+def plotidi(interarr, k):
     idilist = []
-    for m in range(1,k):
-        idilist.append(idi(interarr,m))
-    idi_x = np.linspace(0,k,k-1)
-    plt.plot(idi_x,idilist)
+    for m in range(1, k):
+        idilist.append(idi(interarr, m))
+    idi_x = np.linspace(0, k, k - 1)
+    plt.plot(idi_x, idilist)
     plt.xlabel('lag')
     plt.ylabel('IDI')
     plt.show()
 
 
-def idc(counts,t):
+def idc(counts, t):
     sumlist = []
     for i in range(0, len(counts) - t):
         summa = 0
@@ -153,7 +177,8 @@ def idc(counts,t):
     idcval = variance / (t * np.mean(counts))
     return idcval
 
-def plotidc(counts,t):
+
+def plotidc(counts, t):
     idclist = []
     for m in range(1, t):
         idclist.append(idi(counts, m))
@@ -163,31 +188,29 @@ def plotidc(counts,t):
     plt.ylabel('IDC')
     plt.show()
 
-import argparse
 
 parser = argparse.ArgumentParser(description='Traffic statistics.')
-parser.add_argument('filename', metavar='N', type=str, nargs=1,
+parser.add_argument('filename', metavar='N', type=str,
                     help='filename')
 
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     start_time = time.time()
-    timestamps = []
-    data = []
-    interarrivals = []
-    packetcounter = [0]
+
     args = parser.parse_args()
-    parsefile(args.filename)
-    getinterarrivals(timestamps)
+    timestamps = parsefile(args.filename)
+
+    interarrivals = np.fromiter(getinterarrivals(timestamps),np.float64,len(timestamps)-1)
+
+    packetcounter = getintensity(timestamps)
+    # OK
     #plotinterarrivalpdf(interarrivals)
-    plotintensity(timestamps)
+    #plotintensity(packetcounter)
     #plotpacketcountcorrelation(packetcounter)
+
     #plotarrtimecorrelation(interarrivals)
     #plotidi(interarrivals,50)
     plotidc(packetcounter,200)
-    #print "PMR: %s" % peaktomean(interarrivals)
-    #print "SCV: %s" % scv(interarrivals)
-    #print "Third moment: %s" % thirdmoment(interarrivals)
-    print("Execution took %s seconds" % (time.time() - start_time))
+    print("PMR: {value}".format(value=peaktomean(interarrivals)))
+    print("SCV: {value}".format(value=scv(interarrivals)))
+    print("Third moment: {value}".format(value=thirdmoment(interarrivals)))
+    print("Execution took {value} seconds".format(value=(time.time() - start_time)))

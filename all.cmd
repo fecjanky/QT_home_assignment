0ad1ae4100f5b1a614d508741be53729fe50ef5b
Ferenc Janky <fecjanky@gmail.com>
2018-04-30 22:47:43 +0200

    forward all arguments in all.cmd

--- all.cmd
+++ all.cmd
@@ -1,12 +1,12 @@
 @setlocal
 @echo off
 
-start "toki1 intensity"  /HIGH  %~dp0\toki1.cmd -i %1 intensity  ^&^& exit
-start "toki1 interarrival" /HIGH  %~dp0\toki1.cmd -i %1 interarrival  ^&^& exit
-start "toki1 stats" /HIGH  %~dp0\toki1.cmd -i %1 stats ^> stats.txt  ^&^& exit
-start "toki1 packetcountcorrelation" /HIGH  %~dp0\toki1.cmd -i %1 packetcountcorrelation  ^&^& exit
-start "toki1 arrtimecorrelation" /HIGH  %~dp0\toki1.cmd -i %1 arrtimecorrelation  ^&^& exit
+start "toki1 intensity"  /HIGH  %~dp0\toki1.cmd -i %* intensity  ^&^& exit
+start "toki1 interarrival" /HIGH  %~dp0\toki1.cmd -i %* interarrival  ^&^& exit
+start "toki1 stats" /HIGH  %~dp0\toki1.cmd -i %* stats ^> stats.txt  ^&^& exit
+start "toki1 packetcountcorrelation" /HIGH  %~dp0\toki1.cmd -i %* packetcountcorrelation  ^&^& exit
+start "toki1 arrtimecorrelation" /HIGH  %~dp0\toki1.cmd -i %* arrtimecorrelation  ^&^& exit
 :: idc and idi calculations happen in the same run because of memoization of windowed sums
-start "toki1 idc" /W /HIGH  %~dp0\toki1.cmd -i %1  idi idc  ^&^& exit
+start "toki1 idc" /W /HIGH  %~dp0\toki1.cmd -i %*  idi idc  ^&^& exit
 
 endlocal

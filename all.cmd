3f80e3d1408297ef8d1e102a41c1d42a47d24368
Ferenc Janky <fecjanky@gmail.com>
2018-04-30 19:52:21 +0200

    idc and idi in the same run

--- all.cmd
+++ all.cmd
@@ -6,7 +6,7 @@ start "toki1 interarrival" /HIGH  %~dp0\toki1.cmd -i %1 interarrival  ^&^& exit
 start "toki1 stats" /HIGH  %~dp0\toki1.cmd -i %1 stats ^> stats.txt  ^&^& exit
 start "toki1 packetcountcorrelation" /HIGH  %~dp0\toki1.cmd -i %1 packetcountcorrelation  ^&^& exit
 start "toki1 arrtimecorrelation" /HIGH  %~dp0\toki1.cmd -i %1 arrtimecorrelation  ^&^& exit
-start "toki1 idi" /HIGH  %~dp0\toki1.cmd -i %1 idi  ^&^& exit
-start "toki1 idc" /W /HIGH  %~dp0\toki1.cmd -i %1 idc  ^&^& exit
+:: idc and idi calculations happen in the same run because of memoization of windowed sums
+start "toki1 idc" /W /HIGH  %~dp0\toki1.cmd -i %1  idi idc  ^&^& exit
 
 endlocal

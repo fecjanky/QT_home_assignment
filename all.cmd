7e01a7dfb4ad9c8e0e66aa91600d0fd1855d4686
Ferenc Janky <fecjanky@gmail.com>
2018-04-30 12:08:56 +0200

    removed affinity mask

--- all.cmd
+++ all.cmd
@@ -1,12 +1,12 @@
 @setlocal
 @echo off
 
-start "toki1 intensity" /B /AFFINITY 1 /HIGH  %~dp0\toki1.cmd -i %1 intensity
-start "toki1 interarrival" /B /AFFINITY 2 /HIGH  %~dp0\toki1.cmd -i %1 interarrival
-start "toki1 stats" /B /AFFINITY 4 /HIGH  %~dp0\toki1.cmd -i %1 stats ^> stats.txt
-start "toki1 packetcountcorrelation" /B  /AFFINITY 8 /HIGH  %~dp0\toki1.cmd -i %1 packetcountcorrelation
-start "toki1 arrtimecorrelation" /B /AFFINITY 1 /HIGH  %~dp0\toki1.cmd -i %1 arrtimecorrelation
-start "toki1 idi" /B /AFFINITY 2 /HIGH  %~dp0\toki1.cmd -i %1 idi
-start "toki1 idc" /B /W /AFFINITY 4 /HIGH  %~dp0\toki1.cmd -i %1 idc 
+start "toki1 intensity" /B  /HIGH  %~dp0\toki1.cmd -i %1 intensity
+start "toki1 interarrival" /B /HIGH  %~dp0\toki1.cmd -i %1 interarrival
+start "toki1 stats" /B /HIGH  %~dp0\toki1.cmd -i %1 stats ^> stats.txt
+start "toki1 packetcountcorrelation" /B  /HIGH  %~dp0\toki1.cmd -i %1 packetcountcorrelation
+start "toki1 arrtimecorrelation" /B /HIGH  %~dp0\toki1.cmd -i %1 arrtimecorrelation
+start "toki1 idi" /B /HIGH  %~dp0\toki1.cmd -i %1 idi
+start "toki1 idc" /B /W /HIGH  %~dp0\toki1.cmd -i %1 idc 
 
 endlocal

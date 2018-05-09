9cea2fbf3c11613c9ab0509125926a9b806dd926
Ferenc Janky <fecjanky@gmail.com>
2018-05-09 21:05:43 +0200

    added setup and gitignore

---
+++ ha2/setup.cmd
@@ -0,0 +1,6 @@
+@setlocal
+@echo off
+
+python -m venv venv
+start "install" /B /W venv\Scripts\activate ^&^& python -m pip install --upgrade pip ^&^& pip install matplotlib numpy scipy seaborn pandas networkx
+endlocal
